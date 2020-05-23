ada3d9fedf9a68e8cdf77c19757201cfd32c3dc8
pdavinunes <p.davi.nunes@gmail.com>
2020-05-23 19:48:15 -0300

    Mapeando usuário

--- minhasfinancas/src/main/java/com/dev/minhasfinancas/model/entity/Usuario.java
+++ minhasfinancas/src/main/java/com/dev/minhasfinancas/model/entity/Usuario.java
@@ -1,4 +1,81 @@
 package com.dev.minhasfinancas.model.entity;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table( name = "usuario", schema = "financas")
 public class Usuario {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario)) return false;
+        Usuario usuario = (Usuario) o;
+        return getId().equals(usuario.getId()) &&
+                getNome().equals(usuario.getNome()) &&
+                getEmail().equals(usuario.getEmail()) &&
+                getSenha().equals(usuario.getSenha());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNome(), getEmail(), getSenha());
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
 }
