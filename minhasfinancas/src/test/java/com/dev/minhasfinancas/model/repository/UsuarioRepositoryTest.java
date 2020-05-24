f9de96771085b27525a0e777000c96bbdd971ca0
pdavinunes <p.davi.nunes@gmail.com>
2020-05-24 15:05:35 -0300

    Realizando teste de integração com Usuario

---
+++ minhasfinancas/src/test/java/com/dev/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.dev.minhasfinancas.model.repository;
+
+import com.dev.minhasfinancas.model.entity.Usuario;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class UsuarioRepositoryTest {
+
+    @Autowired
+    UsuarioRepository repository;
+
+    @Test
+    public void deveVerificarAExistenciaDeUmEmail(){
+        //cenario
+        Usuario usuario = Usuario.builder()
+                    .nome("usuario")
+                    .email("usuario@email.com").build();
+
+        repository.save(usuario);
+
+        //ação/ execução
+        boolean result = repository.existsByEmail("usuario@email.com");
+
+        //verificação
+        Assertions.assertTrue(result);
+    }
+
+}
