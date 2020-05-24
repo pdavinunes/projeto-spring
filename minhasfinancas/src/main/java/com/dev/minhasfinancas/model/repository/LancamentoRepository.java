b30337cc44382fbcef33ec236c99ec3195bad920
pdavinunes <p.davi.nunes@gmail.com>
2020-05-24 13:28:31 -0300

    Adicionando JPA's repositorys ao projeto

---
+++ minhasfinancas/src/main/java/com/dev/minhasfinancas/model/repository/LancamentoRepository.java
@@ -0,0 +1,7 @@
+package com.dev.minhasfinancas.model.repository;
+
+import com.dev.minhasfinancas.model.entity.Lancamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class LancamentoRepository extends JpaRepository<Lancamento, Long> {
+}
---
+++ minhasfinancas/src/main/java/com/dev/minhasfinancas/model/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.dev.minhasfinancas.model.repository;
+
+import com.dev.minhasfinancas.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
