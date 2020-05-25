7332bbdb2369d3da0cb1cf2e3c57f0d0c2e0edea
pdavinunes <p.davi.nunes@gmail.com>
2020-05-25 09:46:03 -0300

    Criando o controller de Usuario

---
+++ minhasfinancas/src/main/java/com/dev/minhasfinancas/api/resource/UsuarioResource.java
@@ -0,0 +1,47 @@
+package com.dev.minhasfinancas.api.resource;
+
+import com.dev.minhasfinancas.api.dto.UsuarioDTO;
+import com.dev.minhasfinancas.exception.ErroAutenticacao;
+import com.dev.minhasfinancas.exception.RegraNegocioException;
+import com.dev.minhasfinancas.model.entity.Usuario;
+import com.dev.minhasfinancas.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioResource {
+
+
+    private UsuarioService service;
+
+    public UsuarioResource(UsuarioService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/autenticar")
+    public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
+        try{
+            Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+            return ResponseEntity.ok(usuarioAutenticado);
+        }catch (ErroAutenticacao e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody UsuarioDTO dto){
+        Usuario usuario = Usuario.builder()
+                            .nome(dto.getNome())
+                            .email(dto.getEmail())
+                            .senha(dto.getSenha()).build();
+
+        try{
+            Usuario usuarioSalvo = service.salvarUsuario(usuario);
+            return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+        } catch (RegraNegocioException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
