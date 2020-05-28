ef0da536e998945e45f4e15f6fda11980a41b1cb
pdavinunes <p.davi.nunes@gmail.com>
2020-05-28 09:40:51 -0300

    Construido um componente qualquer

--- minhas-financas-app/src/App.js
+++ minhas-financas-app/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    numero1: null,
+    numero2: null,
+    resultado: null
+  }
+
+  somar = () => {
+    this.setState({
+      resultado: parseInt(this.state.numero1) + parseInt(this.state.numero2)
+    })
+  }
+
+  render (){
+    return(
+      <div>
+        <label>numero1:</label>
+        <input type="number" value={this.state.numero1} 
+        onChange={(e) => this.setState({numero1: e.target.value}) } /><br />
+        <label>numero2:</label>
+        <input type="number" value={this.state.numero2} 
+        onChange={(e) => this.setState({numero2: e.target.value}) } /><br />
+
+
+        <button 
+          onClick={this.somar}>
+              Somar
+        </button><br/>
+        O nome digitado foi: {this.state.resultado}        
+
+
+      </div>
+    )
+  }
 }
 
 export default App;
