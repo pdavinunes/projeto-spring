db59bdab8d5925a76dd816ffc42d4ac278a79fd2
pdavinunes <p.davi.nunes@gmail.com>
2020-05-28 09:43:49 -0300

    Instalando bootswatch

--- minhas-financas-app/src/App.js
+++ minhas-financas-app/src/App.js
@@ -1,39 +1,8 @@
 import React from 'react';
 
 class App extends React.Component {
-
-  state = {
-    numero1: null,
-    numero2: null,
-    resultado: null
-  }
-
-  somar = () => {
-    this.setState({
-      resultado: parseInt(this.state.numero1) + parseInt(this.state.numero2)
-    })
-  }
-
-  render (){
-    return(
-      <div>
-        <label>numero1:</label>
-        <input type="number" value={this.state.numero1} 
-        onChange={(e) => this.setState({numero1: e.target.value}) } /><br />
-        <label>numero2:</label>
-        <input type="number" value={this.state.numero2} 
-        onChange={(e) => this.setState({numero2: e.target.value}) } /><br />
-
-
-        <button 
-          onClick={this.somar}>
-              Somar
-        </button><br/>
-        O nome digitado foi: {this.state.resultado}        
-
-
-      </div>
-    )
+  render() {
+    return(<></>)
   }
 }
 
