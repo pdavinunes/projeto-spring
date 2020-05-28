0093140ba78a576cda0d2e19581888ad44652c85
pdavinunes <p.davi.nunes@gmail.com>
2020-05-28 12:44:25 -0300

    Criando view de home

--- minhas-financas-app/src/main/rotas.js
+++ minhas-financas-app/src/main/rotas.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch, HashRouter} from 'react-router-dom';
 import Login from '../views/login'
 import CadastroUsuario from '../views/cadastroUsuario'
+import Home from '../views/home';
 
 function Rotas() {
     return (
         <HashRouter>
             <Switch>
                 <Route path="/login" component={Login} />
+                <Route path="/home" component={Home} />
                 <Route path="/cadastro-usuario" component={CadastroUsuario} />
             </Switch>
         </HashRouter>
