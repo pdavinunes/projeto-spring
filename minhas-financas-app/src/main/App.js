032561749e197b5d1e78e9981139f1fca990350b
pdavinunes <p.davi.nunes@gmail.com>
2020-05-28 11:50:13 -0300

    Adicionando rotas ao projeto

--- minhas-financas-app/src/main/App.js
+++ minhas-financas-app/src/main/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Login from './views/login';
-import CadastroUsuario from './views/cadastroUsuario'
+import Rotas from './rotas'
 import 'bootswatch/dist/flatly/bootstrap.css'
-import './custom.css'
+import '../custom.css'
+
 class App extends React.Component {
     render() {
         return(
             <div>
-                <CadastroUsuario />
+                <Rotas />
             </div>
         )
     }
