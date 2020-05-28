71adfa1e9fca17bb276f86c9dd6b5c5c883adb51
pdavinunes <p.davi.nunes@gmail.com>
2020-05-28 11:10:51 -0300

    Criando tela de login do projeto

--- minhas-financas-app/src/App.js
+++ minhas-financas-app/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import Login from './views/login';
+import 'bootswatch/dist/flatly/bootstrap.css'
+import './custom.css'
 class App extends React.Component {
-  render() {
-    return(<></>)
-  }
+    render() {
+        return(
+            <div>
+                <Login />
+            </div>
+        )
+    }
 }
 
 export default App;
