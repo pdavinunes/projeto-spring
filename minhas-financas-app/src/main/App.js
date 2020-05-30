dee686b89caaeb4c3e98af926fc27e08d7c4a708
pdavinunes <p.davi.nunes@gmail.com>
2020-05-30 19:14:30 -0300

    Adicionando o primereact ao projeto

--- minhas-financas-app/src/main/App.js
+++ minhas-financas-app/src/main/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import Rotas from './rotas'
 import Navbar from '../components/navbar'
+import { Button } from 'primereact/button';
 
 import 'toastr/build/toastr.min.js'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.css'
 import 'toastr/build/toastr.css';
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 
 class App extends React.Component {
