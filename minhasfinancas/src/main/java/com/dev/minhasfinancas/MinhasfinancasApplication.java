118230b392bcd10c8b360344028ebe7ae5ea80e0
pdavinunes <p.davi.nunes@gmail.com>
2020-05-31 17:03:21 -0300

    Criando arquivo de configuração web no backend

--- minhasfinancas/src/main/java/com/dev/minhasfinancas/MinhasfinancasApplication.java
+++ minhasfinancas/src/main/java/com/dev/minhasfinancas/MinhasfinancasApplication.java
@@ -7,14 +7,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
-public class MinhasfinancasApplication implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE", "OPTIONS");
-    }
-
+public class MinhasfinancasApplication {
     public static void main(String[] args) {
         SpringApplication.run(MinhasfinancasApplication.class, args);
     }
