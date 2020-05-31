32cf2f32ab0e4d91e026d5bca37bd93942aa5729
pdavinunes <p.davi.nunes@gmail.com>
2020-05-31 13:19:36 -0300

    Refatorando validações no formulário de cadastro de usuário

--- minhas-financas-app/src/app/service/usuarioService.js
+++ minhas-financas-app/src/app/service/usuarioService.js
@@ -1,5 +1,5 @@
 import ApiService from '../apiservice'
-
+import ErroValidacao from '../exception/ErroValidacao';
 class UsuarioService extends ApiService{
 
     constructor(){
@@ -12,6 +12,30 @@ class UsuarioService extends ApiService{
 
     obterSaldoPorUsuario(id){
         return this.get(`/${id}/saldo`)
+    }
+
+    validar(usuario){
+        const erros = []
+        
+        if(!usuario.nome){
+            erros.push('O campo Nome é obrigatório')
+        }
+
+        if(!usuario.email){
+            erros.push('O campo Email é obrigatório')
+        }else if(!usuario.email.match(/^[a-z0-9]+@[a-z0-9]+\.[a-z]/)){
+            erros.push('Informe um email válido')
+        }
+
+        if(!usuario.senha || !usuario.senhaRepeticao){
+            erros.push('Digite a senha 2 vezes')
+        }else if(usuario.senha !== !usuario.senhaRepeticao){
+            erros.push('As senhas não batem')
+        }
+        
+        if(erros && erros.length > 0 ){
+            throw new ErroValidacao(erros)
+        }
     }
 
     salvar(usuario){
