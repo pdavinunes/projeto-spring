b2ae679b7545d6751d638abac492a858f8a98abc
pdavinunes <p.davi.nunes@gmail.com>
2020-05-31 15:26:00 -0300

    Melhorando view de home

--- minhas-financas-app/src/views/home.js
+++ minhas-financas-app/src/views/home.js
@@ -37,12 +37,12 @@ class Home extends React.Component {
                 <a className="btn btn-primary btn-lg" 
                     href="#/cadastro-usuario" 
                     role="button">
-                        <i className="fa fa-users"></i>  
+                        <i className="pi pi-users"></i>   
                         Cadastrar Usuário
                 </a>
                 <a className="btn btn-danger btn-lg" 
                     href="#/cadastro-lancamento" 
-                    role="button"><i className="fa fa-users"></i>  Cadastrar Lançamento</a>
+                    role="button"><i className="pi pi-money-bill"></i>  Cadastrar Lançamento</a>
                 </p>
             </div>
         )
